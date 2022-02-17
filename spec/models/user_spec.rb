7ce18e1b478817387283dd47ba3a92b0b44af882
nadakichi <shogo.oilt@gmail.com>
2022-02-17 16:37:13 +0900

    掲載機能の実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,16 +6,22 @@ RSpec.describe User, type: :model do
   end
 
   describe "ユーザー新規登録" do
-    it "nicknameが空だと登録できない" do
-      binding.pry
-      @user.nickname = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include "Nickname can't be blank"
+    context "新規登録できるとき" do
+      it '全ての項目が入力されていれば登録できる' do
+        expect(@user).to be_valid
+      end
     end
-    it "emailが空だと登録できない" do
-      @user.email =''
-      @user.valid?
-      expect(@user.errors.full_messages).to include "Email can't be blank"
+    context '新規登録できないとき' do
+      it "nicknameが空だと登録できない" do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Nickname can't be blank"
+      end
+      it "emailが空だと登録できない" do
+        @user.email =''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Email can't be blank"
+      end
     end
   end
 end
